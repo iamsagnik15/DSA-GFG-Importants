903c2b2bfd0f25771767e6f005b22df712cf76ce
SAGNIK MONDAL <149965643+iamsagnik15@users.noreply.github.com>
2026-03-31 18:39:13 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Buy Stock with Transaction Fee/buy-stock-with-transaction-fee.cpp
+++ Difficulty: Medium/Buy Stock with Transaction Fee/buy-stock-with-transaction-fee.cpp
@@ -1,14 +1,32 @@
 class Solution {
-public:
-    int maxProfit(vector<int>& prices, int fee) {
-        int buy = INT_MIN;
-        int sell = 0;
-
-        for (int price : prices) {
-            buy = max(buy, sell - price);
-            sell = max(sell, buy + price - fee);
+  public:
+    
+    int solve(int i, bool buy, vector<int>& arr, int& k, vector<vector<int>>& dp) {
+        
+        if (i >= arr.size()) {
+            return 0;
         }
-
-        return sell;
+        
+        if (dp[i][buy] != -1) {
+            return dp[i][buy];
+        }
+        int profit = solve(i+1, buy, arr, k, dp);
+        
+        if (buy) {
+            int ans = -arr[i] - k + solve(i+1, false, arr, k, dp);
+            profit = max(profit, ans);
+        } else {
+            int ans = arr[i] + solve(i+1, true, arr, k, dp);
+            profit = max(profit, ans);
+        }
+        
+        return dp[i][buy] = profit;
+    }
+    int maxProfit(vector<int>& arr, int k) {
+        // Code here
+        int n = arr.size();
+        vector<vector<int>> dp(n+1, vector<int>(2, -1));
+        return solve(0, true, arr, k, dp);
     }
 };
+
