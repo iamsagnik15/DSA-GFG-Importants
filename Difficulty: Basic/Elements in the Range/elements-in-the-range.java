0d5729be2e161eb9c546fbe6a2a2948bd117a5e9
SAGNIK MONDAL <149965643+iamsagnik15@users.noreply.github.com>
2026-05-25 14:47:16 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Elements in the Range/elements-in-the-range.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean checkElements(int start, int end, int[] arr) {
+        // code here
+           int count = 0;
+        
+        for(int num : arr){
+            if(num >= start && num <= end){
+                count++;
+            }
+        }
+        
+        if((end - start + 1) == count) return true;
+        return false;
+    }
+}
